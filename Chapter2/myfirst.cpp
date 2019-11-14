455d1f2d8fb6f74a8e93b6922da0396a538ccfb8
ChallengerCY <790597004@qq.com>
2019-11-14 09:24:16 +0800

    c++程序基本结构

---
+++ Chapter2/myfirst.cpp
@@ -0,0 +1,27 @@
+//
+// Created by 成宇 on 2019/11/14.
+//
+#include <iostream>
+
+int main()
+{
+    using namespace std;
+    cout<<"Come up and C++ me some time";
+    cout<<endl;
+    cout<<"You won't regret it!"<<endl;
+    return 0;
+}
+
+
+/**
+ * 代码学习小结
+ * 行注释由 //标识
+ * #include 预处理编译指令
+ * 函数头 int main()
+ * using namespace 编译指令
+ * 函数体 {...}
+ * cout工具用于显示消息语句
+ * return 用于结束main()函数
+ **/
+
+
