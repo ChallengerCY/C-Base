9963713f26265fae1704e6abe52009bbbdad5a8e
ChallengerCY <790597004@qq.com>
2019-12-12 21:19:20 +0800

    整理c++中的浮点数使用

--- Chapter3/floatnum.cpp
+++ Chapter3/floatnum.cpp
@@ -20,4 +20,12 @@ int main()
     return 0;
 }
 
+/** 本段代码小结
+ * cout.setf()迫使输出使用定点表示法。以便更好的了解精度。它防止程序把较大的值切换为E表示法。并使程序显示到小数点后6位。
+ * cout通常会省略末尾的0，cout.setf覆盖了这种表示
+ * double至少有13位精度值。float至少有6位精度值。
+ * float至少32位，double至少48位。long double至少和double一样多
+ * const 用于定义常量。该常量不可再后续进行修改。
+ * */
+
 
