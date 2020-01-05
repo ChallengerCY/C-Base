70b84ad1b4d4194e0805159890c7510404e49e9f
ChallengerCY <790597004@qq.com>
2020-01-05 13:13:02 +0800

    整理c++分支语句和逻辑运算符

---
+++ chapter6/cmake-build-debug/CMakeFiles/chapter6.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chapter6.dir/main.cpp.o"
+  "chapter6"
+  "chapter6.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chapter6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
